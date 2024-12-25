29e0672a15cc3e1409e70754ef2f6ef55109a463
Vikash Sharma <vikash-sharma@cssoftsolutions.com>
2024-12-26 01:47:58 +0530

    Remove javascripts api calling

--- CapturePro/capture_auth/views.py
+++ CapturePro/capture_auth/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
 
-# Create your views here.
 def signIn(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        api_url = "http://127.0.0.1:8000/api/signin/"
+        response = requests.post(api_url, json={"username": username, "password": password})
+        if response.status_code == 200:
+            return redirect('/adminDashboard/')
+        else:
+            return render(request, "Capture_auth/sign-in.html", {"error": "Invalid credentials."})
     return render(request, "Capture_auth/sign-in.html")
 
+
+
 def signUp(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        api_url = "http://127.0.0.1:8000/api/signup/"
+        response = requests.post(api_url, data={"username": username, "email": email, "password": password})
+
+        if response.status_code == 201:
+            return redirect('/login/') 
+        else:
+            return render(request, "Capture_auth/sign-up.html", {"error": "Signup failed, please try again."})
+    
     return render(request, "Capture_auth/sign-up.html")
 
 def adminDashboard(request):
