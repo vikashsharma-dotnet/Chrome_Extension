ff2b39792d0ff43c088261a053ed2143b90c2f89
Vikas <vikash-sharma@cssoftsolutions.com>
2024-12-27 22:11:46 +0530

    Employee TestCode

--- CapturePro/capture_auth/urls.py
+++ CapturePro/capture_auth/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from capture_auth import views
 
 urlpatterns = [
-path('', views.signIn, name="signIn"), 
+path('',views.employeeDashbord,name="userCemployeedashbordrud"),
+path('signin/', views.signIn, name="signin"), 
 path('signup/',views.signUp,name="signup"),
 path('adminDashboard/',views.adminDashboard,name="adminDashboard"),
 path('userCrud/',views.userCrud,name="userCrud"),
-
-
-
 ]
